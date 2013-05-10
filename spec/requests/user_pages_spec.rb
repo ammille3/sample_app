ff7d1d5d30fa2168670acba640fad5724f1ac413
ammille3 <ammille3@oakland.edu>
2013-05-10 13:24:17 -0400

    chapter 7 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,7 +13,12 @@ describe "signup page" do
     it "should not create a user" do
       expect { click_button submit }.not_to change(User, :count)
     end
-    end
+       describe "after submission" do
+        before { click_button submit }
+            it { should have_selector('title', text: 'Sign up') }
+            it { should have_content('error') }
+            end
+            end
   
   describe "with valid information" do
     before do
@@ -23,9 +28,16 @@ describe "signup page" do
         fill_in "Confirmation",  with: "foobar"
         end
 
-   it "should create a user" do
-    expect {click_button submit }.to change(User, :count).by(1)
-    end
+     it "should create a user" do
+         expect {click_button submit }.to change(User, :count).by(1)
+         end
+     describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com')}
+
+        it {should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        end
     end
 #  it {should have_selector('h1', text: 'Sign up')}
  # it {should have_selector('title', text: full_title('Sign up'))}
