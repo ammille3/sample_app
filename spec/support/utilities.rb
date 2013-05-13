8fea67a2c8cd381c3bd5da1abe45a58c58b7dade
ammille3 <ammille3@oakland.edu>
2013-05-13 15:23:09 -0400

    finish user, edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -10,6 +10,15 @@ page.should have_selector('div.alert.alert-error', text: message)
 end
 end
 
+def sign_in(user)
+    visit signin_path
+    fill_in "Email",     with: user.email
+    fill_in "Password",  with: user.password
+    click_button "Sign in"
+    #sign in when not using Capybara as well
+    cookies[:remember_token] = user.remember_token
+    end
+
 # full_title(page_title)
 #base_title = "Ruby on Rails Tutorial Sample App"
 #if page_title.empty?
